4fc12e8de0014b52bddc016de53f2255916e3fba
Kerry Jiang <kerry-jiang@hotmail.com>
2013-01-06 00:38:48 +0800

    added Silverlight and WindowsPhone project files

---
+++ Crypto/Properties/AssemblyInfo.WP71.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BouncyCastle.Crypto for WindowsPhone 7.1")]
+[assembly: AssemblyDescription("BouncyCastle.Crypto for WindowsPhone 7.1")]
--- Crypto/Properties/AssemblyInfo.cs
+++ Crypto/Properties/AssemblyInfo.cs
@@ -5,11 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Crypto")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Crypto")]
+[assembly: AssemblyCompany("BouncyCastle")]
+[assembly: AssemblyProduct("BouncyCastle.Crypto")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
